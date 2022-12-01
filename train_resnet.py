3d46a71dbc123bed729a3a952d5dfd4908d86b13
gutingfeng <gtf_2000@qq.com>
2022-12-01 14:12:10 +0800

    Increase save freq.

--- train_resnet.py
+++ train_resnet.py
@@ -22,7 +22,7 @@ NUM_STEPS = 40000
 POWER = 0.9
 RANDOM_SEED = 12345
 RESTORE_FROM = os.path.join('pretrain', 'resnet101_base.pth')
-SAVE_PRED_EVERY = 5
+SAVE_PRED_EVERY = 1
 CKPT_DIR = 'checkpoints'
 WEIGHT_DECAY = 0.0005
 
